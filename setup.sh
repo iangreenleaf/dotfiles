204ae6108434f7a8209889c6494c13a7449fd839
Ian Young <ian@iangreenleaf.com>
2015-08-28 15:43:35 -0700

    New setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,4 @@
 #!/bin/bash
 
-if [ "$1" = "--help" -o "$1" = "help" -o "$1" = "--usage" ]; then
-  echo "Usage:"
-  echo "$0 [dotfiles_dir] [home_dir]"
-  exit
-fi
-
-repo=$1
-if [ -z "$repo" ]; then
-  repo=`pwd`
-fi
-
-home=$2
-if [ -z "$home" ]; then
-  home=~
-fi
-
-#TODO test existence first
-ln -s "$repo/vim/rc" "$home/.vimrc"
-ln -s "$repo/vim" "$home/.vim"
-ln -s "$repo/boom/boom.json" "$home/.boom"
-ln -s "$repo/boom/conf" "$home/.boom.conf"
-for f in .ackrc .gitconfig .gitignore .bash_completion .xinitrc .editorconfig; do
-  ln -s "$repo/$f" "$home/$f"
-done
-
-include_cmd="source \"$repo/.bashrc\""
-if [ -z "`grep "$include_cmd" "$home/.bashrc"`" ]; then
-  echo "$include_cmd" >> "$home/.bashrc"
-fi
+rcup -K rcrc
+rcup
