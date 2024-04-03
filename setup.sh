939ae5324dcf93165e10d83236050223e10a0644
Ian Young <ian@iangreenleaf.com>
2024-04-03 12:35:27 -0500

    Revert "Use hard link to manage Karabiner config"
    This reverts commit 6f3dfad2522958d71feb540526a988b9a2040f49.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,3 @@
 
 rcup -K rcrc
 rcup
-
-if uname -a | grep -q "Darwin"; then
-  echo "You might want to run the following command to use the Karabiner config:"
-  echo "\tln ~/.config/karabiner/karabiner.json .karabiner.json"
-fi
